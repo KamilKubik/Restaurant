3c2b7712f7a22c33b7cca98db1d0c8913b7b2b74
KamilKubik <robekub15@wp.pl>
2020-07-13 11:02:26 +0200

    Updates

---
+++ precache-manifest.1e160b585e97a814f87a1e3b624b1992.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22aa454684e9fc0c1c1a802b45dc234b",
+    "url": "/Restaurant/index.html"
+  },
+  {
+    "revision": "48ef30587e8f63d03203",
+    "url": "/Restaurant/static/css/main.84e83c0f.chunk.css"
+  },
+  {
+    "revision": "9ec9bbd32abc3fe20301",
+    "url": "/Restaurant/static/js/2.00ed9345.chunk.js"
+  },
+  {
+    "revision": "a25b5045b43cea69265ae58acdb590d6",
+    "url": "/Restaurant/static/js/2.00ed9345.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48ef30587e8f63d03203",
+    "url": "/Restaurant/static/js/main.22dfdfca.chunk.js"
+  },
+  {
+    "revision": "dee19eed088dced54172",
+    "url": "/Restaurant/static/js/runtime-main.a6b95d91.js"
+  },
+  {
+    "revision": "b31d3dedf3f093d067bbb97d0be621f3",
+    "url": "/Restaurant/static/media/drink-brown.b31d3ded.png"
+  },
+  {
+    "revision": "a62eb151ead6c2e28207098d02713e92",
+    "url": "/Restaurant/static/media/food-brown.a62eb151.png"
+  },
+  {
+    "revision": "4a2a98b1d6492ef7e991f2a90aa7a414",
+    "url": "/Restaurant/static/media/kitchen-brown.4a2a98b1.png"
+  },
+  {
+    "revision": "b4664a026e2c645397e04bf9f684a5d0",
+    "url": "/Restaurant/static/media/reviewPhoto.b4664a02.jpg"
+  },
+  {
+    "revision": "4c9e2dc986b9b8e949742f16e0b364f9",
+    "url": "/Restaurant/static/media/threee.4c9e2dc9.jpg"
+  },
+  {
+    "revision": "22e150cb487f6cfa8167277a7cc13b97",
+    "url": "/Restaurant/static/media/vinyl-brown.22e150cb.png"
+  }
+]);
